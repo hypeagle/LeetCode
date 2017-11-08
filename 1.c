aa9e23267a84fd785cbfd08f14d37d45382289f0
hypeagle <554616332@qq.com>
2017-11-08 11:01:34 +0800

    The LeetCode problem [1,2,3]'s solution.

---
+++ 1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1. Two Sum
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int i, j, flag;
+    int *retNums = (int *)malloc(2 * sizeof(int));
+    
+    flag = 0;
+    for(i = 0; i < numsSize; i++) {
+        for(j = i + 1; j < numsSize; j++) {
+            if(nums[i] + nums[j] == target) {
+                retNums[0] = i;
+                retNums[1] = j;
+                
+                flag = 1;
+                break;
+            }
+        }
+        
+        if(flag == 1) {
+            break;
+        }
+    }
+    
+    return retNums;
+}
+
+int main()
+{
+
+}
---
+++ 2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 2. Add Two Numbers
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* retHead = NULL;
+    struct ListNode* retList = NULL;
+    struct ListNode* tmpList = NULL;
+    struct ListNode *p1, *p2;
+
+    p1 = l1;
+    p2 = l2;
+
+    int value = 0;
+    while(1) {
+        if(p1 == NULL && p2 == NULL) {
+            break;
+        }
+
+        if(p1) {
+            value += p1->val;
+            p1 = p1->next;
+        }
+
+        if(p2) {
+            value += p2->val;
+            p2 = p2->next;
+        }
+
+        tmpList = (struct ListNode*)malloc(sizeof(struct ListNode));
+        tmpList->next = NULL;
+        tmpList->val = value % 10;
+        value = value / 10;
+
+        if(retHead == NULL) {
+            retHead = tmpList;
+            retList = tmpList;
+        } else {
+            retList->next = tmpList;
+            retList = tmpList;
+        }
+    }
+
+    if(value > 0) {
+        tmpList = (struct ListNode*)malloc(sizeof(struct ListNode));
+        tmpList->next = NULL;
+        tmpList->val = value % 10;
+        value = value / 10;
+
+        if(retHead == NULL) {
+            retHead = tmpList;
+            retList = tmpList;
+        } else {
+            retList->next = tmpList;
+            retList = tmpList;
+        }
+    }
+
+    return retHead;
+}
+
+int main()
+{
+    return 0;
+}
