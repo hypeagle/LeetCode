0968f048bcd6d850730e8ef30b43561b8fe92e64
hypeagle <554616332@qq.com>
2017-12-18 17:46:25 +0800

    Add Swift solution. The LeetCode problem [1]'s Swift solution.

---
+++ swift/1.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var retInts: [Int] = [0, 0]
+        let numsLen = nums.count
+
+outLoop: for i in 0..<numsLen {
+            for j in i..<numsLen {
+                if i != j && (nums[i] + nums[j]) == target {
+                    retInts[0] = i;
+                    retInts[1] = j;
+
+                    break outLoop;
+                }
+            }
+        }
+
+        return retInts
+    }
+}
